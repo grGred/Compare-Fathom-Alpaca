ed761074653ac334c062b1f72b4158c08556ffbd
Vladislav <81467635+grGred@users.noreply.github.com>
2023-01-04 14:43:39 +0300

    prettier and some alpacas

--- contracts/main/mocks/MockAlpacaOracle.sol
+++
@@ -1,9 +0,0 @@
-pragma solidity 0.6.12;
-
-import "../interfaces/IAlpacaOracle.sol";
-
-contract MockAlpacaOracle is IAlpacaOracle {
-  function getPrice(address, address) external view override returns (uint256 price, uint256 lastUpdate) {
-    return (0, 0);
-  }
-}
